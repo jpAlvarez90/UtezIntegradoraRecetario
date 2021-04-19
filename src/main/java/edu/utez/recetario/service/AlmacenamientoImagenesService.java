0fc559ea87b43539e61a9a778e838d5444256055
dpaola5235 <dapao14@gmail.com>
2021-04-18 19:32:16 -0500

    30/03/2021 codigos de rror

--- src/main/java/edu/utez/recetario/service/AlmacenamientoImagenesService.java
+++ src/main/java/edu/utez/recetario/service/AlmacenamientoImagenesService.java
@@ -15,17 +15,20 @@ public class AlmacenamientoImagenesService {
 
     private final Path root = Paths.get("uploads");
 
-    @Override
+    private UsuarioService usuarioService;
+
+    //@Override
     public void init() {
         try {
             if (!Files.exists(root))
                 Files.createDirectory(root);
         } catch (IOException e) {
+            usuarioService.codigosError(e.toString());
             throw new RuntimeException("No se pudo inizializar la carpeta de las imagenes");
         }
     }
 
-    @Override
+    //@Override
     public void save(MultipartFile multipartFile) {
         try {
             Files.copy(multipartFile.getInputStream(), this.root.resolve("receta_"+multipartFile.getOriginalFilename()));
@@ -41,6 +44,7 @@ public class AlmacenamientoImagenesService {
                 Files.delete(Paths.get("src/main/resources/static/uploads", imageName));
             }
         } catch (IOException e) {
+            usuarioService.codigosError(e.toString());
             throw new RuntimeException("No se pudo guardar el archivo. Error :: "+e.getMessage());
         }
 
